def9fa286a9f501838b1ccb86201416ed4ffccf1
xmxoxo <xmxoxo@users.noreply.github.com>
2018-09-13 16:04:11 +0800

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+# 改为python 3.7 下可运行
+# modify by xmxoxo 
+import functools
+import pdb
+from functools import reduce
 
 class Perceptron(object):
     def __init__(self, input_num, activator):
@@ -17,7 +22,7 @@ class Perceptron(object):
         '''
         打印学习到的权重、偏置项
         '''
-        return 'weights\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
+        return 'weights\t:%s\nbias\t:%f\n' % (list(self.weights), self.bias)
 
     
     def predict(self, input_vec):
@@ -28,12 +33,23 @@ class Perceptron(object):
         # 变成[(x1,w1),(x2,w2),(x3,w3),...]
         # 然后利用map函数计算[x1*w1, x2*w2, x3*w3]
         # 最后利用reduce求和
+
+        '''
+        # python3.7的 map reduce函数使用方法不同
         return self.activator(
             reduce(lambda a, b: a + b,
-                   map(lambda (x, w): x * w,  
-                       zip(input_vec, self.weights))
-                , 0.0) + self.bias)
- 
+                    map(lambda x: x[0] * x[1],
+                        zip(input_vec, self.weights)
+                       )
+                  ,0.0) + self.bias)
+        '''
+
+        return self.activator(
+            reduce(lambda a, b: a + b,
+                    map(lambda x,w: x * w ,
+                        input_vec, self.weights
+                       )
+                  ,0.0) + self.bias)
     def train(self, input_vecs, labels, iteration, rate):
         '''
         输入训练数据：一组向量、与每个向量对应的label；以及训练轮数、学习率
@@ -62,10 +78,11 @@ class Perceptron(object):
         # 把input_vec[x1,x2,x3,...]和weights[w1,w2,w3,...]打包在一起
         # 变成[(x1,w1),(x2,w2),(x3,w3),...]
         # 然后利用感知器规则更新权重
+        # python3.7 中需要指定返回类型为list
         delta = label - output
-        self.weights = map(
-            lambda (x, w): w + rate * delta * x,
-            zip(input_vec, self.weights))
+        self.weights = list(map(
+            lambda x: x[1] + rate * delta * x[0],
+            zip(input_vec, self.weights)))
         # 更新bias
         self.bias += rate * delta
 
@@ -104,14 +121,14 @@ def train_and_perceptron():
 
 
 if __name__ == '__main__': 
+    print ('训练and感知器')
     # 训练and感知器
     and_perception = train_and_perceptron()
     # 打印训练获得的权重
-    print and_perception
+    print (and_perception)
     # 测试
-    print '1 and 1 = %d' % and_perception.predict([1, 1])
-    print '0 and 0 = %d' % and_perception.predict([0, 0])
-    print '1 and 0 = %d' % and_perception.predict([1, 0])
-    print '0 and 1 = %d' % and_perception.predict([0, 1])
-
+    print ('1 and 1 = %d' % and_perception.predict([1, 1]))
+    print ('0 and 0 = %d' % and_perception.predict([0, 0]))
+    print ('1 and 0 = %d' % and_perception.predict([1, 0]))
+    print ('0 and 1 = %d' % and_perception.predict([0, 1]))
 
